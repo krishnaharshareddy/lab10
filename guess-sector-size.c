a77ca805ecc12d24d7eb4afd0cce34c4fc5bf98f
Shouvanik <shouvanik.chakrabarti@gmail.com>
2015-10-28 15:16:50 +0530

    not working

--- guess-sector-size.c
+++ guess-sector-size.c
@@ -1,2 +1,36 @@
+#include <fcntl.h> 
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/stat.h> 
+#include <sys/types.h> 
+#include <unistd.h>
+#include <sys/time.h>
 
+#define JUMP 10000000
+
+int main(int argc, char* argv[]) {
+ int fd = open("dev/sda",O_RDONLY);
+ int r;
+ off_t pos;
+ long diff;
+ struct timeval init,fin;
+ unsigned char buff[10];
+ srand(time(NULL));
+ r = rand() % 1000000;
+ printf("%d\n",r);
+ pos=lseek(fd,10,SEEK_SET);
+ printf("%d\n",(int)pos);
+ read(fd,buff,10);
+ printf("%d\n",(int)buff[0]);
+ pos=lseek(fd,(off_t)JUMP,SEEK_CUR);
+ printf("%d\n",(int)pos);
+ gettimeofday(&init, 0);
+ read(fd,buff,10);
+ printf("%d\n",(int)buff[0]);
+ gettimeofday(&fin, 0);
+ diff =(fin.tv_sec-init.tv_sec)*1000000+(fin.tv_usec-init.tv_usec);
+ printf("%d %lu\n",JUMP,diff);
+ close(fd);
+}
+  
